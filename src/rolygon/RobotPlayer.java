368a80f600ce085d8667f7cbb2a499927371651e
gruzzlymug <kevin@gruzzlymug.com>
2017-01-10 00:53:40 -0500

    add more basics

--- src/rolygon/RobotPlayer.java
+++ src/rolygon/RobotPlayer.java
@@ -2,39 +2,68 @@ package rolygon;
 import battlecode.common.*;
 
 public strictfp class RobotPlayer {
-	static RobotController rc;
+    static RobotController rc;
 	
-	public static void run(RobotController rc) throws GameActionException {
-		RobotPlayer.rc = rc;
+    public static void run(RobotController rc) throws GameActionException {
+        RobotPlayer.rc = rc;
 		
-		RobotType rtype = rc.getType();
-		switch (rtype) {
-		case ARCHON:
-			runArchon();
-			break;
-		case GARDENER:
-			runGardener();
-			break;
-		case LUMBERJACK:
-			break;
-		case SCOUT:
-			break;
-		case SOLDIER:
-			break;
-		case TANK:
-			break;
-		}
-	}
+        RobotType rtype = rc.getType();
+        switch (rtype) {
+        case ARCHON:
+            runArchon();
+            break;
+        case GARDENER:
+            runGardener();
+            break;
+        case LUMBERJACK:
+            runLumberjack();
+            break;
+        case SCOUT:
+            runScout();
+            break;
+        case SOLDIER:
+            break;
+        case TANK:
+            break;
+        }
+    }
 	
 	private static void runArchon() throws GameActionException {
+		System.out.println("Running Zee Archon!!!");
 		while (true) {
+			Direction dir = randomDirection();
+			if (rc.canBuildRobot(RobotType.GARDENER, dir)) {
+				rc.buildRobot(RobotType.GARDENER, dir);
+			}
 			Clock.yield();
 		}
 	}
 	
 	private static void runGardener() throws GameActionException  {
 		while (true) {
+			Direction dir = randomDirection();
+			if (rc.canBuildRobot(RobotType.LUMBERJACK, dir)) {
+				rc.buildRobot(RobotType.LUMBERJACK, dir);
+			} else if (rc.canBuildRobot(RobotType.SCOUT, dir)) {
+				rc.buildRobot(RobotType.SCOUT, dir);
+			}
 			Clock.yield();
 		}
 	}
+	
+    static void runLumberjack() throws GameActionException {
+        while (true) {
+            Clock.yield();
+        }
+    }
+	
+	static void runScout() throws GameActionException {
+		while (true) {
+		    Clock.yield();
+		}
+    }
+	
+    static Direction randomDirection() {
+        return new Direction((float)Math.random() * 2 * (float)Math.PI);
+    }
 }
