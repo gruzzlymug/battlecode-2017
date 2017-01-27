18677e65377a411c8b24a067fe2d62fbb9788d92
gruzzlymug <kevin@gruzzlymug.com>
2017-01-26 23:28:56 -0500

    refactor mud

--- src/rolygon/RobotPlayer.java
+++ src/rolygon/RobotPlayer.java
@@ -21,84 +21,123 @@ public strictfp class RobotPlayer {
         RobotType rtype = rc.getType();
         switch (rtype) {
             case ARCHON:
-                PredicateSelector archonDodge = new PredicateSelector();
-                archonDodge.addPredicate(new UnderFirePredicate());
-                archonDodge.addNode(new DodgeBulletBehavior());
-
-                PredicateSelector archonAvoid = new PredicateSelector();
-                archonAvoid.addPredicate(new EnemyInRangePredicate());
-                archonAvoid.addNode(new AvoidEnemyBehavior());
-
-                PrioritySelector archonPriorities = new PrioritySelector();
-                archonPriorities.addNode(archonDodge);
-                // TODO remove or fix before using. 644 rounds w/o it
-                //archonPriorities.addNode(archonAvoid);
-                archonPriorities.addNode(new BuildGardenersBehavior());
-                archonPriorities.addNode(new RandomMoveBehavior());
-
-                // perform standard think and then run priorities
-                Sequence archonSequence = new Sequence();
-                archonSequence.addNode(new ArchonThinkBehavior());
-                archonSequence.addNode(archonPriorities);
-
-                BehaviorTree archonTree = new BehaviorTree(archonSequence);
-                runArchon(archonTree);
+                Node archonBehaviors = createArchonBehaviors();
+                runArchon(new BehaviorTree(archonBehaviors));
                 break;
             case GARDENER:
-                PredicateSelector gardenerDodge = new PredicateSelector();
-                gardenerDodge.addPredicate(new UnderFirePredicate());
-                gardenerDodge.addNode(new DodgeBulletBehavior());
-
-                PredicateSelector gardenerAvoid = new PredicateSelector();
-                gardenerAvoid.addPredicate(new EnemyInRangePredicate());
-                gardenerAvoid.addNode(new AvoidEnemyBehavior());
-
-                PrioritySelector gardenerPriorities = new PrioritySelector();
-                gardenerPriorities.addNode(gardenerDodge);
-                // TODO remove or fix before using. 644 rounds w/o it
-                //gardenerPriorities.addNode(gardenerAvoid);
-                gardenerPriorities.addNode(new ManageForestBehavior());
-                gardenerPriorities.addNode(new BuildOrderBehavior());
-                gardenerPriorities.addNode(new RandomMoveBehavior());
-
-                BehaviorTree gardenerTree = new BehaviorTree(gardenerPriorities);
-                runGardener(gardenerTree);
+                Node gardenerBehaviors = createGardenerBehaviors();
+                runGardener(new BehaviorTree(gardenerBehaviors));
                 break;
             case LUMBERJACK:
                 runLumberjack();
                 break;
             case SCOUT:
-                Sequence movement = new Sequence();
-                Behavior seekCorner = new SeekCornerBehavior();
-                movement.addNode(seekCorner);
-                RandomMoveBehavior moveScout = new RandomMoveBehavior();
-                movement.addNode(moveScout);
-                BehaviorTree scoutTree = new BehaviorTree(movement);
-                runScout(scoutTree);
+                Node scoutBehaviors = createScoutBehaviors();
+                runScout(new BehaviorTree(scoutBehaviors));
                 break;
             case SOLDIER:
-                PredicateSelector soldierDodge = new PredicateSelector();
-                soldierDodge.addPredicate(new UnderFirePredicate());
-                soldierDodge.addNode(new DodgeBulletBehavior());
-
-                PredicateSelector soldierAttack = new PredicateSelector();
-                soldierAttack.addPredicate(new EnemyInRangePredicate());
-                Sequence attackSequence = new Sequence();
-                attackSequence.addNode(new RandomMoveBehavior());
-                attackSequence.addNode(new RangedAttackBehavior());
-                soldierAttack.addNode(attackSequence);
-
-                PrioritySelector soldierPriorities = new PrioritySelector();
-                soldierPriorities.addNode(soldierDodge);
-                soldierPriorities.addNode(soldierAttack);
-                soldierPriorities.addNode(new RandomMoveBehavior());
-
-                BehaviorTree soldierTree = new BehaviorTree(soldierPriorities);
-                runSoldier(soldierTree);
+                Node soldierBehaviors = createSoldierBehaviors();
+                runSoldier(new BehaviorTree(soldierBehaviors));
                 break;
             case TANK:
                 break;
         }
+    }
+
+    private static Node createArchonBehaviors() {
+        PredicateSelector archonDodge = new PredicateSelector();
+        archonDodge.addPredicate(new UnderFirePredicate());
+        archonDodge.addNode(new DodgeBulletBehavior());
+
+        PredicateSelector archonAvoid = new PredicateSelector();
+        archonAvoid.addPredicate(new EnemyInRangePredicate());
+        archonAvoid.addNode(new AvoidEnemyBehavior());
+
+        PrioritySelector archonPriorities = new PrioritySelector();
+        archonPriorities.addNode(archonDodge);
+        // TODO remove or fix before using. 644 rounds w/o it
+        //archonPriorities.addNode(archonAvoid);
+        archonPriorities.addNode(new BuildGardenersBehavior());
+        archonPriorities.addNode(new RandomMoveBehavior());
+
+        // perform standard think and then run priorities
+        Sequence archonSequence = new Sequence();
+        archonSequence.addNode(new ArchonThinkBehavior());
+        archonSequence.addNode(archonPriorities);
+
+        return archonSequence;
+    }
+
+    private static Node createGardenerBehaviors() {
+        RobotType[] buildOrder = {
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+            RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+        };
+
+        PredicateSelector gardenerDodge = new PredicateSelector();
+        gardenerDodge.addPredicate(new UnderFirePredicate());
+        gardenerDodge.addNode(new DodgeBulletBehavior());
+
+        PredicateSelector gardenerAvoid = new PredicateSelector();
+        gardenerAvoid.addPredicate(new EnemyInRangePredicate());
+        gardenerAvoid.addNode(new AvoidEnemyBehavior());
+
+        PrioritySelector gardenerPriorities = new PrioritySelector();
+        gardenerPriorities.addNode(gardenerDodge);
+        // TODO remove or fix before using. 644 rounds w/o it
+        //gardenerPriorities.addNode(gardenerAvoid);
+        gardenerPriorities.addNode(new ManageForestBehavior());
+        BuildOrderBehavior builder = new BuildOrderBehavior();
+        builder.setBuildOrder(buildOrder);
+        gardenerPriorities.addNode(builder);
+        gardenerPriorities.addNode(new RandomMoveBehavior());
+
+        return gardenerPriorities;
+    }
+
+    private static Node createScoutBehaviors() {
+        Sequence movement = new Sequence();
+        Behavior seekCorner = new SeekCornerBehavior();
+        movement.addNode(seekCorner);
+        RandomMoveBehavior moveScout = new RandomMoveBehavior();
+        movement.addNode(moveScout);
+        return movement;
+    }
+
+    private static Node createSoldierBehaviors() {
+        PredicateSelector soldierDodge = new PredicateSelector();
+        soldierDodge.addPredicate(new UnderFirePredicate());
+        soldierDodge.addNode(new DodgeBulletBehavior());
+
+        PredicateSelector soldierAttack = new PredicateSelector();
+        soldierAttack.addPredicate(new EnemyInRangePredicate());
+        Sequence attackSequence = new Sequence();
+        attackSequence.addNode(new RandomMoveBehavior());
+        attackSequence.addNode(new RangedAttackBehavior());
+        soldierAttack.addNode(attackSequence);
+
+        PrioritySelector soldierPriorities = new PrioritySelector();
+        soldierPriorities.addNode(soldierDodge);
+        soldierPriorities.addNode(soldierAttack);
+        soldierPriorities.addNode(new RandomMoveBehavior());
+
+        return soldierPriorities;
     }
 
     private static void runArchon(BehaviorTree archonTree) throws GameActionException {
