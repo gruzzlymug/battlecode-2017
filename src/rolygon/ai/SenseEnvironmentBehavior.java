c0b82309336cb21626bb62377ea8bc202ab584bb
gruzzlymug <kevin@gruzzlymug.com>
2017-01-28 19:19:07 -0500

    sense nearby robots

--- src/rolygon/ai/SenseEnvironmentBehavior.java
+++ src/rolygon/ai/SenseEnvironmentBehavior.java
@@ -1,7 +1,6 @@
 package rolygon.ai;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 import ddg.ai.Behavior;
 import ddg.ai.Context;
 
@@ -12,6 +11,14 @@ public class SenseEnvironmentBehavior implements Behavior {
     @Override
     public RunResult run(RobotController rc, Context context) throws GameActionException {
         // TODO set up standard robot environmental knowledge context
+        RobotInfo[] nearbyRobots = rc.senseNearbyRobots();
+        for (RobotInfo robot : nearbyRobots) {
+            int id = robot.getID();
+            RobotType type = robot.getType();
+            double health = robot.getHealth();
+            MapLocation location = robot.getLocation();
+            System.out.println(id + ":" + type + ":" + health + " " + location.x + ", " + location.y);
+        }
         return RunResult.FINISHED;
     }
 }
